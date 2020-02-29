9d642f6966ad20010f9c210c316181a090f9285f
Dalibor Petric <dalibor.petric@quantox.com>
2020-02-29 23:03:58 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. 
+          h
         </p>
+        <h1>hello there from custom docker </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
